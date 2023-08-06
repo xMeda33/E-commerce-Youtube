8a1fcb2e6b341976f6cc190788e3562b6dea7c4d
xMeda33 <midodido20021@gmail.com>
2023-08-06 11:37:45 +0300

    Create React App

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">ON&OFF</a>
+      </header>
+      <main>
+        list products
+      </main>
+    </div>
+  );
+}
+
+export default App;
